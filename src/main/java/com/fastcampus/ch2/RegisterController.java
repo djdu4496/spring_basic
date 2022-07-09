4c2d8ec16448313810ffbf331c8f32da632074f9
djdu4 <djdu4496@gmail.com>
2022-07-09 23:35:24 +0900

    Ch.02 Spring MVC 20.(2)

--- src/main/java/com/fastcampus/ch2/RegisterController.java
+++ src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,5 +1,7 @@
 package com.fastcampus.ch2;
 
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +16,21 @@ public class RegisterController {
 //	}
 //	@RequestMapping(value="/register/save", method=RequestMethod.POST)
 	@PostMapping("/register/save") // 4.3부터
-	public String save(User user, Model model) {
+	public String save(User user, Model model) throws Exception {
 		// 1. 유효성 검사
-		
+		if(!isValid(user)) {
+			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
+			
+//			return "redirect:/register/add?msg="+msg; // URL재작성(rewriting)
+			model.addAttribute("msg", msg);
+			return "redirect:/register/add";
+		}
 		// 2. DB에 신규회원 정보를 저장
 		return "registerInfo";
+	}
+
+	private boolean isValid(User user) {
+		// TODO Auto-generated method stub
+		return true;
 	}
 }
